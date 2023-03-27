880f024c6e3d84e1383aa82b71fabf2223cfb2e7
Gavri Kepets <gkepets@gmail.com>
2023-03-27 11:58:46 -0400

    added infor cards and hosting

---
+++ src/components/infocard/infocard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+function InfoCard() {
+    return (
+        <div>
+            <h3>Title Goes Here</h3>
+            <p>Information goes here.</p>
+        </div>
+    );
+}
+
+export default InfoCard;
