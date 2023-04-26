766866276e6bc973f7775d66c1e1b1117ef885c0
Gavri Kepets <gkepets@gmail.com>
2023-04-25 22:39:57 -0400

    added searchbar

--- client/src/pages/Home/HomePage.jsx
+++ client/src/pages/Home/HomePage.jsx
@@ -1,5 +1,12 @@
+import Searchbar from "../../components/searchbar/searchbar";
+import "./homepage.css"
+
 const HomePage = () => {
-    return <div><h1>Find Your New Apartment Here!</h1></div>;
+    const onSelect = (e) => {
+        console.log(e)
+    }
+
+    return <div className="homepage"><h1>Find Your New Apartment Here!</h1><Searchbar onSelect={onSelect}></Searchbar></div>;
 };
 
 export default HomePage;
