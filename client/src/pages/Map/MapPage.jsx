8f60cc8f8a2c887740ce5205663e9f5f9d8977ab
husam-6 <husam@almanakly.com>
2023-05-04 19:53:34 -0400

    Remove Api key and misc text

--- client/src/pages/Map/MapPage.jsx
+++ client/src/pages/Map/MapPage.jsx
@@ -12,14 +12,6 @@ const containerStyle = {
 };
 
 
-var locations = [[40.77084, -73.952721], 
-            [40.774093, -73.953283],
-            [40.770639, -73.949348],
-            [40.677081, -73.890259],
-            [40.655486, -73.95256],
-            [40.696796, -73.931728]]
-
-
 const center = {
   lat: 40.7831,
   lng: -73.9712
@@ -36,7 +28,7 @@ function MapPage() {
 
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
-    googleMapsApiKey: "AIzaSyDR4uw4APdwKIZhj6wH8DIHpDlQCw67XLQ"
+    googleMapsApiKey: "APIKEY"
   })
 
 
@@ -104,71 +96,3 @@ function MapPage() {
 }
 
 export default MapPage;
-
-
-// import React, { useState } from "react";
-// import { GoogleMap, InfoWindow, Marker } from "@react-google-maps/api";
-
-// const markers = [
-//   {
-//     id: 1,
-//     name: "Chicago, Illinois",
-//     position: { lat: 41.881832, lng: -87.623177 }
-//   },
-//   {
-//     id: 2,
-//     name: "Denver, Colorado",
-//     position: { lat: 39.739235, lng: -104.99025 }
-//   },
-//   {
-//     id: 3,
-//     name: "Los Angeles, California",
-//     position: { lat: 34.052235, lng: -118.243683 }
-//   },
-//   {
-//     id: 4,
-//     name: "New York, New York",
-//     position: { lat: 40.712776, lng: -74.005974 }
-//   }
-// ];
-
-// function Map() {
-//   const [activeMarker, setActiveMarker] = useState(null);
-
-//   const handleActiveMarker = (marker) => {
-//     if (marker === activeMarker) {
-//       return;
-//     }
-//     setActiveMarker(marker);
-//   };
-
-//   const handleOnLoad = (map) => {
-//     const bounds = new window.google.maps.LatLngBounds();
-//     markers.forEach(({ position }) => bounds.extend(position));
-//     map.fitBounds(bounds);
-//   };
-
-//   return (
-//     <GoogleMap
-//       onLoad={handleOnLoad}
-//       onClick={() => setActiveMarker(null)}
-//       mapContainerStyle={{ width: "100vw", height: "100vh" }}
-//     >
-//       {markers.map(({ id, name, position }) => (
-//         <Marker
-//           key={id}
-//           position={position}
-//           onClick={() => handleActiveMarker(id)}
-//         >
-//           {activeMarker === id ? (
-//             <InfoWindow onCloseClick={() => setActiveMarker(null)}>
-//               <div>{name}</div>
-//             </InfoWindow>
-//           ) : null}
-//         </Marker>
-//       ))}
-//     </GoogleMap>
-//   );
-// }
-
-// export default Map;
