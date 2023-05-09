5f5f4febecfb760f907ece250c7d4ca4d8828697
husam-6 <husam@almanakly.com>
2023-05-09 16:19:25 -0400

    Greg clutch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/header/header.jsx'
 import AboutPage from './pages/About/AboutPage'
@@ -7,7 +7,30 @@ import HomePage from './pages/Home/HomePage.jsx';
 
 import './App.css'
 
+
+
 function App() {
+  const SERVER = "http://localhost:5000"
+
+  const [addrs, setAddrs] = useState([]);
+  const [nta, setNta] = useState('');
+  const [address, setAddress] = useState('');
+  
+  // get addresses from backend via get request
+  useEffect(() => {
+    const requestOptions = {
+      method: 'POST',
+      mode: 'cors',
+      headers: { 'Content-Type': 'application/json', 'Accept': 'application/json'},
+      body: JSON.stringify({nta, address })
+    };
+    fetch(SERVER + '/data', requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      setAddrs(data);
+    });
+  }, []);
+
   return (
     <BrowserRouter>
       <Header />
@@ -15,7 +38,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
-          <Route path="/map" element={<MapPage />} />
+          <Route path="/map" element={<MapPage markers={addrs}/>} />
         </Routes>
       </div>
     </BrowserRouter>
