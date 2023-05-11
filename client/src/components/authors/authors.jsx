18939ec01b42976b85d5d5072d0a04f4933a3b13
mbentivegna <mjbentivegna01@gmail.com>
2023-05-11 13:31:30 -0400

    update background

--- client/src/components/authors/authors.jsx
+++ client/src/components/authors/authors.jsx
@@ -10,8 +10,8 @@ function Authors() {
             <div className = "item"><img src="https://raw.githubusercontent.com/gkgkgkgk/StreetEasy-But-Hard/main/client/ayden.png" />Ayden Shankman</div>
             <div className = "item"><img src="https://raw.githubusercontent.com/gkgkgkgk/StreetEasy-But-Hard/main/client/gavri.jpg" />Gavri Kepets</div>
             <div className = "item"><img src="https://raw.githubusercontent.com/gkgkgkgk/StreetEasy-But-Hard/main/client/michael.png" />Michael Bentivegna</div>
-            <div className = "item"><img src="https://raw.githubusercontent.com/gkgkgkgk/StreetEasy-But-Hard/main/client/background_city.png" />Melina Tsai</div>
-            <div className = "item"><img src="https://raw.githubusercontent.com/gkgkgkgk/StreetEasy-But-Hard/main/client/background_city.png" />Cesar Rodriguez</div>
+            <div className = "item"><img src="https://raw.githubusercontent.com/gkgkgkgk/StreetEasy-But-Hard/main/client/melina.jpg" />Melina Tsai</div>
+            <div className = "item"><img src="https://raw.githubusercontent.com/gkgkgkgk/StreetEasy-But-Hard/main/client/cesar.jpg" />Cesar Rodriguez</div>
         </div>
         </div>
     );
