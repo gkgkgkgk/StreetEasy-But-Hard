d81d067f151224c1825b32adbdb41453ebe3306d
husam-6 <husam@almanakly.com>
2023-05-11 02:03:33 -0400

    Fix minor bug with displaying notes with less than 25 words

--- client/src/pages/Map/Info.jsx
+++ client/src/pages/Map/Info.jsx
@@ -11,7 +11,7 @@ function Info(props) {
             <h2  style={{ color: 'red ' }}>You likely can't use a voucher here...</h2>
             <h3  style={{ color: 'black', justifyContent: 'center'}}>Notes from HRI:</h3>
             <p style={{color: 'black'}}>
-                {showMore ? props.element.Notes : `${props.element.Notes.split(" ", 25).join(' ') + "..."}`}
+                {showMore || props.element.Notes.split(" ").length < 25? props.element.Notes : `${props.element.Notes.split(" ", 25).join(' ') + "..."}`}
             </p>
             <div className='cont'>
                 {props.element.Notes.split(" ").length > 25 ?
