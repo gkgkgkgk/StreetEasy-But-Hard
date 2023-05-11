22c34e31cdb8b2988af4b7a935590864af6c6f2c
Gavri Kepets <gkepets@gmail.com>
2023-05-11 18:23:09 -0400

    fixed img

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -6,7 +6,7 @@ function Header(props) {
     return (
         <div className='fixed-header'>
             <div style={{height:"100%", width: "200px", marginTop: "10px"}}>
-                <img style={{filter: "invert(100%)"}} src="../../../streethard.svg" alt="test"></img>
+                <img style={{filter: "invert(100%)"}} src="https://raw.githubusercontent.com/gkgkgkgk/StreetEasy-But-Hard/main/client/streethard.svg" alt="test"></img>
             </div>
             <div>
                 <ul className='navul'>
